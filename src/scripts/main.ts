0d5352c9265f0748af99f5b88ab097dc01e8c844
yoratoni <71235233+yoratoni@users.noreply.github.com>
2024-02-14 13:37:18 +0100

    WIP

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,4 @@
-import logger from "utils/logger";
+import { config } from "dotenv";
 
 
-logger.info("Hello world!");
+config();
---
+++ src/types/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        DATABASE_URL: string
+        DATABASE_NAME: string
+    }
+}
