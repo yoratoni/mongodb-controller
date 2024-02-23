47ce0f8963ad08849d5a980315197972d9e2bac7
yoratoni <71235233+yoratoni@users.noreply.github.com>
2024-02-23 11:21:02 +0100

    WIP

--- src/scripts/controller.ts
+++ src/scripts/controller.ts
@@ -11,18 +11,5 @@ import { addFieldToCollection, findDocuments, renameCollection, renameField } fr
  * @param count The number of documents to process.
  */
 export default async function controller(db: Db, collection: Collection, count: number) {
-    /**
-     * Renamed the field "licenceId" to "licenseId" in the "usages" collection.
-     */
-    // const test = await renameField(collection, {}, "licenceId", "licenseId");
 
-    /**
-     * Renamed the collection "licences" to "licenses".
-     */
-    // await renameCollection(collection, "licenses");
-
-    /**
-     * Added "restricted" field to the "aliases" collection with the default value "false".
-     */
-    // const test = await addFieldToCollection(collection, {}, "restricted", false);
 }
