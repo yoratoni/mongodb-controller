85532945d704716c5fd7b9e315dc1d1b5234445b
yoratoni <yoratoni.dev@gmail.com>
2024-09-18 14:40:28 +0200

    Fix

--- src/migrations/migrateAllProxies.ts
+++ src/migrations/migrateAllProxies.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Collection, Db } from "mongodb"
-import { ObjectId } from "mongodb"
 import type { MigrationInfo } from "types/migration"
-import ProxiesJSON from "../assets/fixProxies.json"
+// import ProxiesJSON from "../assets/fixProxies.json"
 
 /**
  * Information about this migration script.
@@ -19,11 +18,11 @@ export const info__migrateAllProxies: MigrationInfo = {
  * @param _collection The collection.
  * @param _count The number of documents to process.
  */
-export default async function migrateAllProxies(_db: Db, _collection: Collection, _count: number) {
-	for await (const proxy of ProxiesJSON) {
-		await _collection.updateOne(
-			{ _id: new ObjectId(proxy.aliasId) },
-			{ $set: { aliasNFTAddress: proxy.proxyAddress } },
-		)
-	}
+export default function migrateAllProxies(_db: Db, _collection: Collection, _count: number) {
+	// for await (const proxy of ProxiesJSON) {
+	// 	await _collection.updateOne(
+	// 		{ _id: new ObjectId(proxy.aliasId) },
+	// 		{ $set: { aliasNFTAddress: proxy.proxyAddress } },
+	// 	)
+	// }
 }
